8635e78613cad22f1b8e80f4c73daa9dfbdde81f
greatroar <61184462+greatroar@users.noreply.github.com>
2022-04-08 10:51:35 +0200

    amd64: load mask into register
    The Go assembler doesn't seem to guarantee alignment of GLOBL data
    sections and I couldn't find the directive for forcing it. This patch
    loads the polymask constant into a register with MOVOU before using it,
    just like the amd64 asm in the stdlib crypto module does with its
    constants.
    
    Performance is largely unchanged:
    
    name          old speed      new speed      delta
    Polyval_1-8    805MB/s ± 1%   815MB/s ± 2%  +1.29%  (p=0.001 n=14+15)
    Polyval_4-8    934MB/s ± 1%   936MB/s ± 1%    ~     (p=0.155 n=13+14)
    Polyval_8-8   1.82GB/s ± 1%  1.82GB/s ± 1%    ~     (p=0.870 n=15+15)
    Polyval_16-8  1.88GB/s ± 1%  1.88GB/s ± 1%    ~     (p=0.347 n=15+12)
    Polyval_32-8  1.91GB/s ± 1%  1.90GB/s ± 1%    ~     (p=0.085 n=15+14)
    Polyval_64-8  1.93GB/s ± 2%  1.92GB/s ± 1%  -0.52%  (p=0.036 n=14+12)

--- polyval_amd64.s
+++ polyval_amd64.s
@@ -15,35 +15,36 @@ TEXT ·polymulAsm(SB), NOSPLIT, $0-16
 	MOVQ  key+8(FP), CX
 	MOVOU (AX), X0
 	MOVOU (CX), X1
+	MOVOU polymask<>+0(SB), X2
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X0, X2
-	PXOR      X0, X2
-	PSHUFD    $0xee, X1, X3
-	PXOR      X1, X3
-	PCLMULQDQ $0x00, X2, X3
-	MOVOU     X0, X2
-	PCLMULQDQ $0x11, X1, X2
+	PSHUFD    $0xee, X0, X3
+	PXOR      X0, X3
+	PSHUFD    $0xee, X1, X4
+	PXOR      X1, X4
+	PCLMULQDQ $0x00, X3, X4
+	MOVOU     X0, X3
+	PCLMULQDQ $0x11, X1, X3
 	PCLMULQDQ $0x00, X1, X0
 
 	// Karatsuba 2
 	MOVOU      X0, X1
-	SHUFPS     $0x4e, X2, X1
-	MOVOU      X2, X4
-	PXOR       X0, X4
-	PXOR       X1, X4
-	PXOR       X3, X4
-	MOVHLPS    X4, X2
-	PUNPCKLQDQ X4, X0
+	SHUFPS     $0x4e, X3, X1
+	MOVOU      X3, X5
+	PXOR       X0, X5
+	PXOR       X1, X5
+	PXOR       X4, X5
+	MOVHLPS    X5, X3
+	PUNPCKLQDQ X5, X0
 
 	// Montgomery reduce
-	MOVOU     polymask<>+0(SB), X1
+	MOVOU     X2, X1
 	PCLMULQDQ $0x00, X0, X1
 	PSHUFD    $0x4e, X1, X1
 	PXOR      X0, X1
-	XORPS     X1, X2
-	PCLMULQDQ $0x11, polymask<>+0(SB), X1
-	PXOR      X2, X1
+	XORPS     X1, X3
+	PCLMULQDQ $0x11, X2, X1
+	PXOR      X3, X1
 	MOVOU     X1, (AX)
 	RET
 
@@ -54,44 +55,45 @@ TEXT ·polymulBlocksAsm(SB), NOSPLIT, $0-32
 	MOVQ  pow+8(FP), CX
 	MOVQ  input+16(FP), DX
 	MOVQ  nblocks+24(FP), BX
-	MOVOU (AX), X0
+	MOVOU polymask<>+0(SB), X0
+	MOVOU (AX), X1
 	MOVQ  BX, SI
 	ANDQ  $0x07, SI
 	JZ    initWideLoop
-	MOVOU 112(CX), X1
+	MOVOU 112(CX), X2
 
 singleLoop:
-	MOVOU (DX), X2
-	PXOR  X0, X2
+	MOVOU (DX), X3
+	PXOR  X1, X3
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X2, X3
-	PXOR      X2, X3
-	PSHUFD    $0xee, X1, X0
-	PXOR      X1, X0
-	PCLMULQDQ $0x00, X3, X0
-	MOVOU     X2, X3
-	PCLMULQDQ $0x11, X1, X3
-	PCLMULQDQ $0x00, X1, X2
+	PSHUFD    $0xee, X3, X4
+	PXOR      X3, X4
+	PSHUFD    $0xee, X2, X1
+	PXOR      X2, X1
+	PCLMULQDQ $0x00, X4, X1
+	MOVOU     X3, X4
+	PCLMULQDQ $0x11, X2, X4
+	PCLMULQDQ $0x00, X2, X3
 
 	// Karatsuba 2
-	MOVOU      X2, X4
-	SHUFPS     $0x4e, X3, X4
 	MOVOU      X3, X5
-	PXOR       X2, X5
-	PXOR       X4, X5
-	PXOR       X0, X5
-	MOVHLPS    X5, X3
-	PUNPCKLQDQ X5, X2
+	SHUFPS     $0x4e, X4, X5
+	MOVOU      X4, X6
+	PXOR       X3, X6
+	PXOR       X5, X6
+	PXOR       X1, X6
+	MOVHLPS    X6, X4
+	PUNPCKLQDQ X6, X3
 
 	// Montgomery reduce
-	MOVOU     polymask<>+0(SB), X0
-	PCLMULQDQ $0x00, X2, X0
-	PSHUFD    $0x4e, X0, X0
-	PXOR      X2, X0
-	XORPS     X0, X3
-	PCLMULQDQ $0x11, polymask<>+0(SB), X0
-	PXOR      X3, X0
+	MOVOU     X0, X1
+	PCLMULQDQ $0x00, X3, X1
+	PSHUFD    $0x4e, X1, X1
+	PXOR      X3, X1
+	XORPS     X1, X4
+	PCLMULQDQ $0x11, X0, X1
+	PXOR      X4, X1
 	ADDQ      $0x10, DX
 	SUBQ      $0x01, SI
 	JNZ       singleLoop
@@ -101,169 +103,169 @@ initWideLoop:
 	JZ   done
 
 wideLoop:
-	PXOR X1, X1
-	PXOR X3, X3
 	PXOR X2, X2
+	PXOR X4, X4
+	PXOR X3, X3
 
 	// Block 7
-	MOVOU 112(DX), X4
-	MOVOU 112(CX), X5
+	MOVOU 112(DX), X5
+	MOVOU 112(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 6
-	MOVOU 96(DX), X4
-	MOVOU 96(CX), X5
+	MOVOU 96(DX), X5
+	MOVOU 96(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 5
-	MOVOU 80(DX), X4
-	MOVOU 80(CX), X5
+	MOVOU 80(DX), X5
+	MOVOU 80(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 4
-	MOVOU 64(DX), X4
-	MOVOU 64(CX), X5
+	MOVOU 64(DX), X5
+	MOVOU 64(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 3
-	MOVOU 48(DX), X4
-	MOVOU 48(CX), X5
+	MOVOU 48(DX), X5
+	MOVOU 48(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 2
-	MOVOU 32(DX), X4
-	MOVOU 32(CX), X5
+	MOVOU 32(DX), X5
+	MOVOU 32(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 1
-	MOVOU 16(DX), X4
-	MOVOU 16(CX), X5
+	MOVOU 16(DX), X5
+	MOVOU 16(CX), X6
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X6
-	PXOR      X4, X6
 	PSHUFD    $0xee, X5, X7
 	PXOR      X5, X7
-	PCLMULQDQ $0x00, X6, X7
-	MOVOU     X4, X6
-	PCLMULQDQ $0x11, X5, X6
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X6, X1
-	PXOR      X4, X3
+	PSHUFD    $0xee, X6, X8
+	PXOR      X6, X8
+	PCLMULQDQ $0x00, X7, X8
+	MOVOU     X5, X7
+	PCLMULQDQ $0x11, X6, X7
+	PCLMULQDQ $0x00, X6, X5
 	PXOR      X7, X2
+	PXOR      X5, X4
+	PXOR      X8, X3
 
 	// Block 0
-	MOVOU (DX), X4
-	MOVOU (CX), X5
-	PXOR  X0, X4
+	MOVOU (DX), X5
+	MOVOU (CX), X6
+	PXOR  X1, X5
 
 	// Karatsuba 1
-	PSHUFD    $0xee, X4, X0
-	PXOR      X4, X0
-	PSHUFD    $0xee, X5, X6
-	PXOR      X5, X6
-	PCLMULQDQ $0x00, X0, X6
-	MOVOU     X4, X0
-	PCLMULQDQ $0x11, X5, X0
-	PCLMULQDQ $0x00, X5, X4
-	PXOR      X0, X1
-	PXOR      X4, X3
-	PXOR      X6, X2
+	PSHUFD    $0xee, X5, X1
+	PXOR      X5, X1
+	PSHUFD    $0xee, X6, X7
+	PXOR      X6, X7
+	PCLMULQDQ $0x00, X1, X7
+	MOVOU     X5, X1
+	PCLMULQDQ $0x11, X6, X1
+	PCLMULQDQ $0x00, X6, X5
+	PXOR      X1, X2
+	PXOR      X5, X4
+	PXOR      X7, X3
 
 	// Karatsuba 2
-	MOVOU      X3, X0
-	SHUFPS     $0x4e, X1, X0
-	MOVOU      X1, X4
-	PXOR       X3, X4
-	PXOR       X0, X4
-	PXOR       X2, X4
-	MOVHLPS    X4, X1
-	PUNPCKLQDQ X4, X3
+	MOVOU      X4, X1
+	SHUFPS     $0x4e, X2, X1
+	MOVOU      X2, X5
+	PXOR       X4, X5
+	PXOR       X1, X5
+	PXOR       X3, X5
+	MOVHLPS    X5, X2
+	PUNPCKLQDQ X5, X4
 
 	// Montgomery reduce
-	MOVOU     polymask<>+0(SB), X0
-	PCLMULQDQ $0x00, X3, X0
-	PSHUFD    $0x4e, X0, X0
-	PXOR      X3, X0
-	XORPS     X0, X1
-	PCLMULQDQ $0x11, polymask<>+0(SB), X0
-	PXOR      X1, X0
+	MOVOU     X0, X1
+	PCLMULQDQ $0x00, X4, X1
+	PSHUFD    $0x4e, X1, X1
+	PXOR      X4, X1
+	XORPS     X1, X2
+	PCLMULQDQ $0x11, X0, X1
+	PXOR      X2, X1
 	ADDQ      $0x80, DX
 	SUBQ      $0x01, BX
 	JNZ       wideLoop
 
 done:
-	MOVOU X0, (AX)
+	MOVOU X1, (AX)
 	RET
