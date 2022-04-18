a3c50a7574e13b54ec3ce5aecffda98301027023
Eric Lagergren <eric@ericlagergren.com>
2022-04-18 00:02:02 -0700

    simplify benchmarks
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- polyval_arm64_test.go
+++ polyval_arm64_test.go
@@ -3,31 +3,16 @@
 package polyval
 
 import (
+	"fmt"
 	"testing"
 )
 
-func BenchmarkPolyvalNoSHA3_1(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 1)
-}
-
-func BenchmarkPolyvalNoSHA3_4(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 4)
-}
-
-func BenchmarkPolyvalNoSHA3_8(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 8)
-}
-
-func BenchmarkPolyvalNoSHA3_16(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 16)
-}
-
-func BenchmarkPolyvalNoSHA3_32(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 32)
-}
-
-func BenchmarkPolyvalNoSHA3_64(b *testing.B) {
-	benchmarkPolyvalNoSHA3(b, 64)
+func BenchmarkPolyvalNoSHA3(b *testing.B) {
+	for _, n := range benchBlocks {
+		b.Run(fmt.Sprintf("%d", n*16), func(b *testing.B) {
+			benchmarkPolyvalNoSHA3(b, n)
+		})
+	}
 }
 
 func benchmarkPolyvalNoSHA3(b *testing.B, nblocks int) {
